874fa78e71b0bd5a492b29445a83bc77e0878bee
Siim Heinla <siimhei@greeny.cs.tlu.ee>
2015-09-09 12:38:25 +0300

    vägev

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+
+	// LOGIN.PHP
+	
+?>
+<html>
+<head>
+	<title>Login Page</title>
+</head>
+
+<body>
+	<h2>Log in</h2>
+	
+		<input type="email" placeholder="E-post"> <br><br>
+		<input type="password" placeholder="Parool"> <br><br>
+		<input type="submit" value="log in"> <br><br>
+	<h2>Create user</h2>
+</body>
+
+
+</html>
