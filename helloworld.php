c640a486b5c8ba54be2506a8484238f3863a45ac
Siim Heinla <siimhei@greeny.cs.tlu.ee>
2015-09-09 11:33:37 +0300

    veel koodi

--- helloworld.php
+++ helloworld.php
@@ -4,3 +4,16 @@
 	
 	echo $first_name." ".$last_name;
 ?>
+<pr>
+<?php
+
+	$age = 5;
+	
+	//testib loogikat, juhul kui vanus on vähem kui 18 siis kirjuta alaealine
+	//muul juhul trüki täisealine.
+	// if'i sisse loogikatehe
+	if($age < 18){echo "alaealine"}
+		//tõene
+	else{echo "täisealine"};
+	
+?>
