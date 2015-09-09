2771faf7f99a2f94737ddf2e1d4fd3d11d21f6f3
Siim Heinla <siimhei@greeny.cs.tlu.ee>
2015-09-09 13:22:14 +0300

    ja edasi

--- login.php
+++ login.php
@@ -1,7 +1,26 @@
 <?php
 
 	// LOGIN.PHP
-	
+	$email_error = "";
+	$passw_error = "";
+	//echo $_POST["email"];
+	// kontrollime et keegi vajutas input nuppu
+	if($_SERVER["REQUEST_METHOD"]=="POST"){
+		//echo "keegi vajutas nuppu";
+		//kontrollin et e-post ei ole tühi
+		if (empty($_POST["email"])){
+			$email_error = "see väli on kohustulik";
+		}
+		if (empty($_POST["password"])){
+			$passw_error = "see väli on kohustulik";
+		} else {
+			
+			//kui oleme siia jõudnud, siis parool pole tühi
+			if(strlen($_POST["password"]) < 8){
+				$passw_error="peab olema vähemalt 8 tähemärki";
+			}
+		}
+	}
 ?>
 <html>
 <head>
@@ -11,9 +30,11 @@
 <body>
 	<h2>Log in</h2>
 	
-		<input type="email" placeholder="E-post"> <br><br>
-		<input type="password" placeholder="Parool"> <br><br>
-		<input type="submit" value="log in"> <br><br>
+		<form action="login.php" method="post">
+			<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?><br><br>
+			<input name="password" type="password" placeholder="Parool"> <?php echo $passw_error; ?> <br><br>
+			<input type="submit" value="log in"> <br><br>
+		</form>
 	<h2>Create user</h2>
 </body>
 
